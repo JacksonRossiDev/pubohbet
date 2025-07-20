581e36e480f0e19acdcd71e508e56ee3fac273ec
Jackson Rossi <jacksonrossi@Jacksons-MacBook-Pro-2.local>
2025-07-20 13:44:31 -0500

    m

--- app.config.js
+++ app.config.js
@@ -8,31 +8,24 @@ export default {
     icon: './assets/ohbet-icon-final.png',
     userInterfaceStyle: 'light',
     platforms: ['ios', 'android', 'web'],
-    updates: {
-      fallbackToCacheTimeout: 0
-    },
+    updates: { fallbackToCacheTimeout: 0 },
     assetBundlePatterns: ['**/*'],
     plugins: [
       'expo-dev-client',
       [
         'expo-notifications',
-        {
-          icon: './assets/ohbet-icon-final.png',
-          color: '#000000'
-        }
+        { icon: './assets/ohbet-icon-final.png', color: '#000000' }
       ],
       [
         'expo-camera',
-        {
-          cameraPermission: 'Allow $(PRODUCT_NAME) to access your camera.'
-        }
+        { cameraPermission: 'Allow $(PRODUCT_NAME) to access your camera.' }
       ],
       'expo-tracking-transparency',
       'expo-dev-menu'
     ],
     ios: {
       bundleIdentifier: 'com.spl.ohbetappfinal',
-      buildNumber: '126',
+      buildNumber: '127',
       supportsTablet: true,
       icon: './assets/ohbet-icon-final.png',
       infoPlist: {
@@ -40,6 +33,7 @@ export default {
         NSUserNotificationUsageDescription: 'We use notifications to keep you updated.',
         UIBackgroundModes: ['remote-notification']
       },
+      // Force production entitlement
       entitlements: {
         'aps-environment': 'production'
       }
@@ -62,13 +56,7 @@ export default {
       androidMode: 'default',
       androidCollapsedTitle: 'New Notification'
     },
-    web: {
-      favicon: './assets/favicon.png'
-    },
-    extra: {
-      eas: {
-        projectId: '72c1f9c2-2829-45e3-bac6-20bc0c2e992a'
-      }
-    }
+    web: { favicon: './assets/favicon.png' },
+    extra: { eas: { projectId: '72c1f9c2-2829-45e3-bac6-20bc0c2e992a' } }
   }
 };
